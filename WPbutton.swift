e2db1596841d3190fda3232ceecdf06bcf762cc7
unc-dan <firstdan8989@gmail.com>
2020-04-21 18:28:50 +0700

    Custom button
    WPbutton
    
    -Created configure() function
    -Created initializer for backgroundColor and title

---
+++ WPbutton.swift
@@ -0,0 +1,36 @@
+//
+//  WPbutton.swift
+//  Workout52-P
+//
+//  Created by Dan T on 21/04/2020.
+//  Copyright © 2020 Dan T. All rights reserved.
+//
+
+import UIKit
+
+class WPbutton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false //this just means use autolayout = false
+    }
+
+}
